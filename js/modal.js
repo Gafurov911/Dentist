a2b58ccd080996fda2e97f03ebc8a76421835b64
Gafurov911 <165022251+Gafurov911@users.noreply.github.com>
2024-04-13 14:24:55 +0500

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -23,3 +23,38 @@ document.getElementById("my-modal").addEventListener('click', event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove('open');
 });
+
+// Modal Form------------------------------
+document.getElementById('sign_form').addEventListener('submit', checkForm)
+const domains = ["google", "yandex", "mail", "gmail", "inbox"]
+function checkForm(event) {
+    event.preventDefault()
+    let el = document.getElementById('sign_form');
+    let name = el.name.value;
+    let email = el.email.value;
+    let pass = el.pass.value;
+    let repass = el.repass.value;
+    
+
+    let fail = '';
+
+    if (name == "" || email == "" || pass == "")
+        fail = 'Заполните все поля';
+    else if (name.length <= 2 || name.length > 6)
+        fail = 'Мин и Макс кол-во символов: 2 - 6';
+    else if (pass !== repass)
+        fail = 'Пароли не совпадают';
+    else if (pass.split('&').length > 1)
+        fail = 'Некоректный пароль';
+
+
+
+    if (fail != "")
+        document.getElementById('error').innerHTML = fail;
+    else {
+        document.getElementById('error').innerHTML = '';
+        alert('Форма успешно заполнена!')
+    }
+
+    return false;
+}
